19bd24b197a0aefc9e2380c1f4c375384781065d
Richard Mena <richardmf9425@yahoo.com>
2019-12-09 14:22:20 -0500

    added second endpoint for sightings

--- client/src/List.js
+++ client/src/List.js
@@ -1,32 +1,60 @@
-import React, { useEffect } from 'react';
-
+import React, { useEffect, useState, Fragment } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import axios from 'axios';
 function List({ flowers }) {
+	const [ show, setShow ] = useState(false);
+	const [ flowerModal, setFlowerModal ] = useState({});
+	const [ sightings, setSightings ] = useState([]);
+	const handleShow = (flower) => {
+		setShow(true);
+		axios.get(`/api/sightings/${flower.COMNAME}`).then((res) => setSightings(res.data));
+		setFlowerModal(flower);
+	};
+	const handleClose = () => setShow(false);
+	console.log(sightings);
 	const flowersList = flowers.map((flower, index) => (
-		<tr key={index}>
+		<tr key={index} onClick={() => handleShow(flower)}>
 			<td>{flower.COMNAME}</td>
 		</tr>
 	));
 	console.log(flowers);
 	return (
-		<div className="landing-section">
-			<div className="flowers-table">
-				<div className="table-headers">
-					<div className="row">
-						<div className="card shadow">
-							<div className="tableWrapper">
-								<h1>Flowers</h1>
-								<table className="table table-striped">
-									<thead>
-										<td>Common Name:</td>
-									</thead>
-									<tbody>{flowersList}</tbody>
-								</table>
+		<Fragment>
+			<div className="landing-section">
+				<div className="flowers-table">
+					<div className="table-headers">
+						<div className="row">
+							<div className="card shadow">
+								<div className="tableWrapper">
+									<h1>Flowers</h1>
+									<table className="table table-striped">
+										<thead>
+											<td>Common Name:</td>
+										</thead>
+										<tbody>{flowersList}</tbody>
+									</table>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+
+			<Modal show={show} onHide={handleClose}>
+				<Modal.Header closeButton>
+					<Modal.Title>Flower: {flowerModal.COMNAME}</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<table>
+						<thead>
+							<td>Person:</td>
+							<td>Location:</td>
+							<td>Date :</td>
+						</thead>
+					</table>
+				</Modal.Body>
+			</Modal>
+		</Fragment>
 	);
 }
 
--- routes/routes.js
+++ routes/routes.js
@@ -22,8 +22,26 @@ router.get('/', async (req, res) => {
 	}
 });
 
-router.get('/sightings/:id', async (req, res) => {
-	let sql = `SELECT PERSON, LOCATION, SIGHTED FROM SIGHTINGS WHERE NAME = ? LIMIT 10`;
+// Get 10 most recent sightings for a flower by common name
+router.get('/sightings/:name', async (req, res) => {
+	let sql = `SELECT PERSON, LOCATION, SIGHTED FROM SIGHTINGS WHERE NAME = ? ORDER BY SIGHTED DESC LIMIT 10`;
+	let params = req.params.name;
+	console.log('name in backend: ' + params);
+	try {
+		db.all(sql, params, (err, rows) => {
+			if (err) {
+				res.status(400).send(err.message);
+				return;
+			}
+			res.json({
+				message: 'success',
+				data: rows
+			});
+		});
+	} catch (e) {
+		console.error(e.message);
+		res.status(500).send('Server Error');
+	}
 });
 
 module.exports = router;
